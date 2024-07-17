e0bf2d40de9e6cadc042e73983d77dc93beb288e
sehyeon <0494shd@naver.com>
2024-07-17 12:54:37 +0900

    사장님 페이지

--- situ/reservations/views.py
+++ situ/reservations/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from .models import Seat
 from django.contrib.auth.decorators import login_required
 from datetime import timedelta
+import pytz
 
 @login_required
 def seat_overview(request):
@@ -12,9 +13,10 @@ def seat_overview(request):
 @login_required
 def update_seat_status(request, seat_id, status):
     seat = get_object_or_404(Seat, id=seat_id)
+    korea_tz = pytz.timezone('Asia/Seoul')
     if status == 'occupied':
         seat.seat_status = 'occupied'
-        seat.seat_start_time = timezone.now()
+        seat.seat_start_time = timezone.now().astimezone(korea_tz)
     elif status == 'reserved':
         seat.seat_status = 'reserved'
         seat.reserved_by = request.user
@@ -50,3 +52,15 @@ def seat_check(request, seat_id):
         seat.seat_start_time = timezone.now()
     seat.save()
     return redirect('seat_overview')
+
+# 좌석 데이터가 있는지 확인하는 디버깅
+@login_required
+def seat_overview(request):
+    seats = Seat.objects.all()
+    if not seats:
+        print("No seats found.")
+    else:
+        for seat in seats:
+            print(f"Seat {seat.seats_no}: Status - {seat.get_seat_status_display()}")
+    return render(request, 'reservations/seat_overview.html', {'seats': seats, 'user': request.user})
+
--- situ/situ/settings.py
+++ situ/situ/settings.py
@@ -127,11 +127,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
+
+USE_TZ = True
 
 USE_I18N = True
 
-USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
