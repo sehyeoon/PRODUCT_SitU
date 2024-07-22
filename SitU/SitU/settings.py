c1435146ee71933d404b2b8dc32c12b9ced4aeb6
01tilinfinity <kuwinter0209@gmail.com>
2024-07-22 21:37:31 +0900

    feat:please

--- SitU/SitU/settings.py
+++ SitU/SitU/settings.py
@@ -3,6 +3,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -121,14 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    BASE_DIR / 'main' / 'static',
-    BASE_DIR / 'dashboard' / 'static',
-    BASE_DIR / 'reservations' / 'static',
+    STATIC_DIR,
 ]
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
